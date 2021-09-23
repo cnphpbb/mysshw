efc835a429f2199f7925ca4f451771f813407504
cnphpbb <moqiruyi@gmail.com>
2021-09-24 01:36:52 +0800

    :construction: todo::新的配置文件

---
+++ config/config.go
@@ -0,0 +1,16 @@
+/**
+ * @Author   DenysGeng <cnphp@hotmail.com>
+ *
+ * @Description: 采用toml做为新版本的配置文件
+ * @Version: 	1.0.0
+ * @Date:     	2021/9/23
+ */
+
+package config
+
+/**
+ * config example:
+ * cfg_dir = ~/.sshw.toml  //默认 $HOME/.sshw.toml
+ * [sys]
+ *
+ */
