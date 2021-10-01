c014b71ee9126ee74355c0340b284278e8db85a5
moqiruyi <moqiruyi@gmail.com>
2021-10-02 05:55:06 +0800

    :sparkles:完善toml配置文件解析

--- config/config.go
+++ config/config.go
@@ -10,39 +10,91 @@ package config
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os/user"
+	"path"
+	"strings"
+
 	"github.com/BurntSushi/toml"
 )
 
 type (
 	Configs struct {
-		CfgDir  string
-		SyncCfg *SyncCfg
-		Nodes *Nodes
+		CfgDir  string   `toml: "cfg_dir"`
+		SyncCfg *SyncCfg `toml: "sysc"`
+		Nodes   *Nodes   `toml: "nodes"`
 	}
 
 	SyncCfg struct {
-		Type        string
-		RemoteUri   string
-		UserName    string
-		Password    string
-		KeyPath     string
-		Passphrase  string
-		RemotePath  string
-		AccessToken string
+		Type        string `toml: "type"`
+		RemoteUri   string `toml: "remote_uri"`
+		UserName    string `toml: "username"`
+		Password    string `toml: "password"`
+		KeyPath     string `toml: "keyPath"`
+		Passphrase  string `toml: "passphrase"`
+		RemotePath  string `toml: "remote_path"`
+		AccessToken string `toml: "access_token"`
+		GistID      string `tome: "gist_id"`
 	}
 	Nodes struct {
-		Groups   string
-		SSHNodes []*SSHNode
+		Groups   string	`tome:"groups"`
+		SSHNodes []*SSHNode `toml:"ssh"`
 	}
 	SSHNode struct {
-		Name       string
-		Alias      string
-		Host       string
-		User       string
-		Port       int
-		KeyPath    string
-		Passphrase string
-		Password   string
+		Name       string `toml:"name"`
+		Alias      string `toml:"alias"`
+		Host       string `toml:"host"`
+		User       string `toml:"user"`
+		Port       int		`toml:"port"`
+		KeyPath    string	`toml:"keypath"`
+		Passphrase string	`toml:"passphrase"`
+		Password   string `toml:"password"`
 	}
 )
 
+var (
+	CFG_PATH string = "~/.sshw.toml"
+	//APP_VER  string = "2.5.6.1211"
+	//LOG_PATH  = "mysshw.log"
+	CFG *Configs
+)
+
+
+func LoadConfigBytes(sshwpath string) ([]byte, error) {
+	var cfgPath string
+	u,err := user.Current()
+	if err != nil {
+		return  nil, err
+	}
+	_cfgs := strings.SplitAfter(sshwpath, "/")
+	if _cfgs[0] == "~" {
+		cfgPath = u.HomeDir
+	} else {
+		cfgPath = _cfgs[0]
+	}
+	if _cfgs[1] != ".sshw.toml" {
+		CFG_PATH = path.Join(cfgPath, _cfgs[1])
+	} else {
+		CFG_PATH = path.Join(cfgPath, ".sshw.toml")
+	}
+	cfgBytes,err := ioutil.ReadFile(CFG_PATH)
+	if err == nil {
+		return cfgBytes, nil
+	}
+	return nil, err
+}
+
+func LoadConfig() error {
+	cfgBytes, err := LoadConfigBytes(CFG_PATH)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(cfgBytes))
+	var c *Configs
+	err = toml.Unmarshal(cfgBytes, &c)
+	if err != nil {
+		return err
+	}
+	CFG = c
+	return nil
+}
