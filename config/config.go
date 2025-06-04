cf752344ceb5391f31d233a470bd0ac0e5045b1b
cnphpbb <moqiruyi@gmail.com>
2025-06-04 15:17:24 +0800

    feat: 添加配置文件备份功能

--- config/config.go
+++ config/config.go
@@ -16,6 +16,7 @@ import (
 	"path"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/BurntSushi/toml"
 	"github.com/spf13/viper"
@@ -216,3 +217,29 @@ func isCfgPath(cfgPath string) (dir, file, ext string) {
 
 //todo:
 //func WriteViperConfig() error {}
+
+// BackupConfig creates a timestamped backup of the configuration file
+func BackupConfig() error {
+	cfgPath, err := GetCfgPath(CFG_PATH)
+	if err != nil {
+		return fmt.Errorf("failed to get config path: %v", err)
+	}
+
+	// Read original file
+	content, err := os.ReadFile(cfgPath)
+	if err != nil {
+		return fmt.Errorf("failed to read config file: %v", err)
+	}
+
+	// Create backup filename with timestamp
+	timestamp := time.Now().Format("20060102_150405")
+	backupPath := cfgPath + "." + timestamp + ".bak"
+
+	// Write backup file
+	err = os.WriteFile(backupPath, content, 0644)
+	if err != nil {
+		return fmt.Errorf("failed to write backup file: %v", err)
+	}
+
+	return nil
+}
