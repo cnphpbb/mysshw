c3afed2f9880d9ca70b092111677f43ba48d72b7
cnphpbb <moqiruyi@gmail.com>
2025-08-30 07:24:04 +0800

    fix: 根据操作系统类型显示不同的退出描述
    添加系统兼容性检查，为Windows和Unix系统分别显示不同的退出提示信息

--- cmd/messages.go
+++ cmd/messages.go
@@ -3,7 +3,8 @@ package cmd
 const (
 	RunSSHClientLoginSessionEndCallbackStr = "SSH session ended, returning to main menu..."
 	GlobalScreenClearingStr                = "\033[H\033[2J"
-	GlobalExitingDescStr                   = `Press "Ctrl+C" and then press "q | Q" or "Ctrl+D" to exit, or select the SSH node:`
+	GlobalExitingDescUnixStr               = `Press "Ctrl+C" and then press "q | Q" or "Ctrl+D" to exit, or select the SSH node:`
+	GlobalExitingDescWindowsStr            = `Press "Ctrl+C" and then press "q | Q" to exit, or select the SSH node:`
 	GlobalExitingStr                       = "Exiting..."
 	RunSSHCtrlCHintStr                     = "\nReceived termination signal, mysshw:: Exiting..."
 	RunSSHCtrlCResultStr                   = "\nReceived termination signal, mysshw:: Exiting..."
